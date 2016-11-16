e543ff1fb937df6ce300a3598d8eb4e914a2b203
Ahmad Arsyad Bin. Ahmad <arsh90@gmail.com>
2016-11-16 16:53:25 +0800

    Just removed whitespace

--- app/src/main/java/com/example/thisi/applicationx/MainMenuActivity.java
+++ app/src/main/java/com/example/thisi/applicationx/MainMenuActivity.java
@@ -279,7 +279,6 @@ public class MainMenuActivity extends Activity implements IWsdl2CodeEvents {
                                 pctrl.DAYEND = false; 
 
                                 mydb.deleteAndInsertPOSControl(db, pctrl); 
-
                             }
 
 
