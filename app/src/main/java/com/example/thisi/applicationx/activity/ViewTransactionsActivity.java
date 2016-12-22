ab68d8b4330869fab9d40fe7e2bcb0d7d42debc8
Ahmad Arsyad Bin. Ahmad <arsh90@gmail.com>
2016-12-22 16:40:47 +0800

    Listing out headers in ViewTransactionsActivity

--- app/src/main/java/com/example/thisi/applicationx/activity/ViewTransactionsActivity.java
+++ app/src/main/java/com/example/thisi/applicationx/activity/ViewTransactionsActivity.java
@@ -49,96 +49,75 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addTransactionsToView() {
-    //     TableLayout tableLayout = (TableLayout) findViewById(R.id.tableTransactions);
-    //     // Add header row
-    //     // TableRow rowHeader = createHeaderRow();
-    //     // tableLayout.addView(rowHeader);
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.tableTransactions);
+        // Add header row
+        // TableRow rowHeader = createHeaderRow();
+        // tableLayout.addView(rowHeader);
 
-    //     SQLiteDatabase db = dataHelper.getReadableDatabase();
-    //     // Start the transaction.
-    //     db.beginTransaction();
+        SQLiteDatabase db = dataHelper.getReadableDatabase();
+        // Start the transaction.
+        db.beginTransaction();
 
-    //     try {
-    //         String todaysDateInString = new SimpleDateFormat("yyyyMMdd").format(new Date());
+        try {
+            String todaysDateInString = new SimpleDateFormat("yyyyMMdd").format(new Date());
 
-    //         String selectQuery = "SELECT * FROM header " +
-    //                 "WHERE trans_date = '" + todaysDateInString + 
-    //                 "' ORDER BY TRANS_TIME DESC;";
+            String selectQuery = "SELECT * FROM header " +
+                    "WHERE trans_date = '" + todaysDateInString + // get today's transactions only
+                    "' ORDER BY TRANS_TIME DESC;";
                     
-    //         Cursor cursor = db.rawQuery(selectQuery, null);
+            Cursor cursor = db.rawQuery(selectQuery, null);
 
-    //         int rowCount = cursor.getCount();
+            int rowCount = cursor.getCount();
 
-    //         if (rowCount > 0) {
-    //             int i = 0;
+            if (rowCount > 0) {
+                int i = 0;
 
-    //             while (cursor.moveToNext()) {
-    //                 i++;
+                while (cursor.moveToNext()) {
+                    i++;
 
-    //                 // Read columns data
-    //                 int outlet_id = i;
-    //                 String customerCode = cursor.getString(cursor.getColumnIndex("CUSTOMER_CODE"));
-    //                 String transTime = cursor.getString(cursor.getColumnIndex("TRANS_TIME"));
-    //                 String rcpNo = cursor.getString(cursor.getColumnIndex("RCP_NO"));
+                    // Read columns data
+                    int outlet_id = i;
+                    String customerCode = cursor.getString(cursor.getColumnIndex("CUSTOMER_CODE"));
+                    String transTime = cursor.getString(cursor.getColumnIndex("TRANS_TIME"));
+                    String rcpNo = cursor.getString(cursor.getColumnIndex("RCP_NO"));
                     
-    //                 // dara rows
-    //                 TableRow row = new TableRow(this);
+                    // dara rows
+                    TableRow row = new TableRow(this);
 
-    //                 if (i % 2 == 0) {
-    //                     row.setBackgroundColor(Color.parseColor(evenRowColor));
-    //                 } else {
-    //                     row.setBackgroundColor(Color.parseColor(oddRowColor));
-    //                 }
+                    if (i % 2 == 0) {
+                        row.setBackgroundColor(Color.parseColor(evenRowColor));
+                    } else {
+                        row.setBackgroundColor(Color.parseColor(oddRowColor));
+                    }
 
-    //                 row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
-    //                         TableLayout.LayoutParams.WRAP_CONTENT));
+                    row.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT,
+                            TableLayout.LayoutParams.WRAP_CONTENT));
 
-    //                 if(rmColumnValue == null)
-    //                     rmColumnValue = "0.0";
+                    String[] colText = { transTime, customerCode, rcpNo, " "};
 
-    //                 BigDecimal bigDecimalRMColumnValue = new BigDecimal(rmColumnValue);
+                    for (String text : colText) {
+                        TextView tv = new TextView(this);
+                        tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+                                TableRow.LayoutParams.WRAP_CONTENT));
 
-    //                 DecimalFormat df = new DecimalFormat("0.00");
-    //                 df.setMaximumFractionDigits(2);
-    //                 String stringRmColumnValue = df.format(bigDecimalRMColumnValue);
+                        tv.setTextSize(11);
+                        tv.setPadding(5, 5, 5, 5);
+                        tv.setText(text);
+                        row.addView(tv);
+                    }
+                    tableLayout.addView(row);
+                }
+                cursor.close();
 
-    //                 String[] colText = {outlet_id + "", nameColumnValue, stringRmColumnValue, " "};
+                
+            }
+            db.setTransactionSuccessful();
 
-    //                 int j = 0;
-    //                 for (String text : colText) {
-    //                     TextView tv = new TextView(this);
-    //                     tv.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
-    //                             TableRow.LayoutParams.WRAP_CONTENT));
-
-    //                     if (j == 2) { // RM column align right
-    //                         tv.setGravity(Gravity.RIGHT);
-    //                     } else {
-    //                         tv.setGravity(Gravity.LEFT);
-    //                     }
-
-    //                     tv.setTextSize(11);
-    //                     tv.setPadding(5, 5, 5, 5);
-    //                     tv.setText(text);
-    //                     row.addView(tv);
-
-    //                     j++;
-    //                 }
-    //                 tableLayout.addView(row);
-    //             }
-
-    //             cursor.close();
-
-    //             latest_row_after_suspend_inserts = i;
-    //         }
-
-    //         db.setTransactionSuccessful();
-
-
-    //     } catch (SQLiteException e) {
-    //         e.printStackTrace();
-    //     } finally {
-    //         db.endTransaction();
-    //         db.close();
-    //     }
-    // }
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        } finally {
+            db.endTransaction();
+            db.close();
+        }
+    }
 }
